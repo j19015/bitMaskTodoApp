bd236b07685be29cf35ff25afebf3625c2e6d4d3
j19015 <j19015@sangi.jp>
2023-11-22 21:48:23 +0900

    [add] モデルの記述を追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,28 @@
 class Task < ApplicationRecord
+    
+  validates :name, presence: true
+  validates :body, presence: true
+  validates :is_loop, presence: true
+  
+  # ヘルパーメソッドとして定義
+  def display_is_loop(is_loop_value)
+    options = [
+      "月",
+      "火",
+      "水",
+      "木",
+      "金",
+      "土",
+      "日",
+    ]
+
+    selected_values = []
+
+    options.each_with_index do |option, index|
+      bit = 2**index
+      selected_values << option if is_loop_value & bit == bit
+    end
+
+    selected_values.join(", ")
+  end
 end

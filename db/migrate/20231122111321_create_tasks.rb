7d69fedbd0b9bb798786f9c64a6108a2d5224221
j19015 <j19015@sangi.jp>
2023-11-22 21:45:56 +0900

    [add] カラムの追加

--- db/migrate/20231122111321_create_tasks.rb
+++ db/migrate/20231122111321_create_tasks.rb
@@ -1,7 +1,13 @@
 class CreateTasks < ActiveRecord::Migration[6.1]
   def change
     create_table :tasks do |t|
-
+      # タスク名
+      t.string :name
+      # 内容
+      t.string :body
+      # 周期
+      t.integer :is_loop
+      # 作成日時 更新日時
       t.timestamps
     end
   end
